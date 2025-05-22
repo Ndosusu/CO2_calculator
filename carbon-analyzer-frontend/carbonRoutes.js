528a178d1e19c84b80883c9a220c412f1fc9d2d6
ndossant <n.dossantos@outlook.fr>
2025-05-22 13:35:32 +0200

    deleted

--- carbon-analyzer-frontend/carbonRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/carbonRoutes.js
-const express = require('express');
-const router = express.Router();
-const { calculateCarbon } = require('../../carbon-analyzer-frontend/carbonController');
-
-router.post('/calculate', calculateCarbon);
-
-module.exports = router;
